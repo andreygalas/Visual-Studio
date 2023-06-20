e9a70d278c184d27643ebdfc4569fff263f3f28d
andreygalas <andreygalas2022@gmail.com>
2023-06-20 16:51:33 -0700

    comate

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,12 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
         }
     }
 }
